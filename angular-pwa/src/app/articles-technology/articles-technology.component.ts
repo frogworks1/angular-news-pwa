b471b61f0bb5466b7f8a443f1473503befc4a9f3
frogworks1 <frogworks1@gmail.com>
2019-06-01 17:37:50 -0700

    Commit news site

---
+++ angular-pwa/src/app/articles-technology/articles-technology.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NewsapiService } from '../services/newsapi.service';
+
+
+@Component({
+  selector: 'app-articles-technology',
+  templateUrl: './articles-technology.component.html',
+  styleUrls: ['./articles-technology.component.css']
+})
+export class ArticlesTechnologyComponent {
+  articles$: Observable<any>;
+
+  constructor(private newsapi: NewsapiService) {}
+
+  ngOnInit() {
+    // technology news articles
+    this.articles$ = this.newsapi.getArticlesTechnology();
+  }
+}
---
+++ angular-pwa/src/app/services/newsapi.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsapiService {
+  api_key = '084116cdcca24abbb6311ad2b1c6cf3b';
+
+  constructor(private http: HttpClient) {}
+
+  getArticlesTechnology(): Observable<any> {
+    return this.http
+      .get(
+        'https://newsapi.org/v2/top-headlines?category=technology&language=en&country=us&apiKey=' +
+          this.api_key
+      )
+      .pipe(map((data: any) => data.articles));
+  }
+
+  getArticlesJavaScript(): Observable<any> {
+    return this.http
+      .get(
+        'https://newsapi.org/v2/everything?q=javascript&sortBy=latest&apiKey=' +
+          this.api_key
+      )
+      .pipe(map((data: any) => data.articles));
+  }
+}
